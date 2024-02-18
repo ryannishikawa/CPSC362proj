e4fd6d457caedec733c65d69c29c857a2ea831b4
Matt De Binion <debinionm@gmail.com>
2024-02-18 15:03:29 -0800

    Simple database setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log("This is the server starting.");
+import express from 'express';
+import sqlite3 from 'sqlite3';
+
+const server = express();
+const PORT = process.env.PORT || 5000;
+const db = new sqlite3.Database('./data/credentials.db');
+
+server.get('/api/data', (req, res) => {
+    db.all('SELECT * FROM user_data', (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error' });
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`The server is running and listening on port ${PORT}`);
+});
