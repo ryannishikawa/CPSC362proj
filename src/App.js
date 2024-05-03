ad1b37b4581763c0267a31b64873b2e814c2086c
Matt De Binion <debinionm@gmail.com>
2024-05-02 22:17:15 -0700

    Updated docs

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-//****************************************************************************************************************************
-//Program name: "App.js".  This program is the main part of our web app. Copyright (C)  *
-//2024 Ryan Nishikawa                                                                                                        *
-//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
-//version 3 as published by the Free Software Foundation.                                                                    *
-//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
-//warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
-//A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                           *
-//****************************************************************************************************************************
-
-
-//=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
-//
-//Author information
-//  Author names: Ryan Nishikawa,
-//  Author emails: ryannishikawa48@csu.fullerton.edu,
-//  Course ID: CPSC362
-//
-//Program information
-//  Program name: Task manager
-//  Date of last update: February 15, 2024
-//  Programming language(s): JavaScript, HTML, CSS
-//  Files in this program: App.js, login.js, register.js, home.js, etc...
-//
-//  OS of the computer where the program was developed: Ubuntu 22.04.3 LTS
-//  OS of the computer where the program was tested: Ubuntu 22.04.3 LTS
-//  Status: WIP
-//
-//References for this program
-//  https://www.youtube.com/watch?v=psU13XU1gDY&list=LL&index=3&t=796s&ab_channel=CodeWithViju
-//
-//Purpose
-//  The hub of everything
-//
-//This file
-//   File name: App.js
-//   Date of last update: February 15, 2024
-//   Languages: JavaScript, HTML, CSS
-//
-//References for this file
-//   https://www.youtube.com/watch?v=psU13XU1gDY&list=LL&index=3&t=796s&ab_channel=CodeWithViju
-//
-//=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2**
-//
-//
-//
-//
-//===== Begin code area ================================================================================================
-
+/**
+ * @file App.js
+ * @author Kyle Ho <kyleho@csu.fullerton.edu>
+ * @author Ryan Nishikawa <ryannishikawa48@csu.fullerton.edu>
+ * @author Matt De Binion <mattdb@csu.fullerton.edu>
+ * @description This file is responsible for routing to specific pages within our app.
+ * 
+ * @see {@link: https://github.com/ryannishikawa/CPSC362proj} for our project repository and the README.md file within the server
+ * directory for a less stressful viewing experience.
+ */
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
